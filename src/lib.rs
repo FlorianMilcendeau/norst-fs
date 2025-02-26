2b7f1f740504cf7054b076eda4fa46e9eb422ae8
Florian Milcendeau <fmilcendeau85@gmail.com>
2025-02-26 23:00:48 +0100

    feat: write and read file

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,50 @@
+#![deny(clippy::all)]
 
+use napi::{bindgen_prelude::*, tokio::fs};
+
+#[macro_use]
+extern crate napi_derive;
+
+#[napi]
+async fn read_file_async(path: String) -> Result<Buffer> {
+  match fs::read(path).await {
+    Ok(value) => Ok(value.into()),
+    Err(error) => Err(Error::new(
+      Status::GenericFailure,
+      format!("failed to read file, {}", error),
+    )),
+  }
+}
+
+#[napi]
+fn read_file(path: String) -> Result<Buffer> {
+  match std::fs::read(path) {
+    Ok(value) => Ok(value.into()),
+    Err(error) => Err(Error::new(
+      Status::GenericFailure,
+      format!("failed to read file, {}", error),
+    )),
+  }
+}
+
+#[napi]
+fn write_file(path: String, contents: Buffer) -> Result<()> {
+  match std::fs::write(path, contents) {
+    Ok(_) => Ok(()),
+    Err(error) => Err(Error::new(
+      Status::GenericFailure,
+      format!("failed to write file, {}", error),
+    )),
+  }
+}
+
+#[napi]
+async fn write_file_async(path: String, contents: Buffer) -> Result<()> {
+  match fs::write(path, contents).await {
+    Ok(_) => Ok(()),
+    Err(error) => Err(Error::new(
+      Status::GenericFailure,
+      format!("failed to write file, {}", error),
+    )),
+  }
+}
